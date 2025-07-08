18819fa9c947a8cbf33d117a5c91bbf46d133467
mathuc <mathuchinnamurugan@gmail.com>
2025-07-08 14:24:44 +0530

    update commit

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI,
+    mongoUrl: process.env.MONGO_URL,
     collectionName: 'sessions',
   }),
   cookie: {
@@ -115,3 +115,10 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/todo-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
