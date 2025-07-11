591490d73e4201f8470d93ef94f5106c4ee919df
mathuc <mathuchinnamurugan@gmail.com>
2025-07-11 15:59:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     // Check if user is authenticated
-    fetch(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/auth/user`, {
+    fetch(`${'https://online-todo-mathu.onrender.com'}/auth/user`, {
       credentials: 'include',
     })
       .then(res => res.ok ? res.json() : null)
@@ -17,11 +17,11 @@ function App() {
   }, []);
 
   const handleGoogleSignIn = () => {
-    window.location.href = `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/auth/google`;
+    window.location.href = `${'https://online-todo-mathu.onrender.com'}/auth/google`;
   };
 
   const handleLogout = () => {
-    fetch(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/auth/logout`, {
+    fetch(`${'https://online-todo-mathu.onrender.com'}/auth/logout`, {
       credentials: 'include',
     }).then(() => window.location.reload());
   };
