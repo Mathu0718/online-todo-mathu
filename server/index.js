395c817ba023c54a391321a802f65e921a326bca
mathuc <mathuchinnamurugan@gmail.com>
2025-07-11 14:22:52 +0530

    new 6

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.set('trust proxy', 1);
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_UR,
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 app.use(express.json());
